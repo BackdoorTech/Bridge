352fba3913ed1f688e3710d578ad515eaee532b6
Peter Maquiran <peter.maquiran@equilibrium.local>
2021-08-02 20:30:13 +0100

    workaround to fix prettyJs 'orresponding type declarations'

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-// import prettyJs from "pretty-js";
+// https://github.com/cucumber/cucumber-js/issues/1152
+// import prettgyJs  from 'pretty-js'
+
+// Need to load with require because it has no corresponding type declarations
+const prettyJs = require('pretty-js');
 
 export const bridge = async (code: string, data: any) => {
   code = code
     .replace(/[\r\n\s]+{/g, "{")
     .replace(/if[\r\n\s]+[(]/g, "if(")
-    .replace(/[\s]+ =/g, " =")
-    .replace(/= [\s]+/g, "= ")
     .replace(/"/g, `'`);
 
-  // code = prettyJs(code);
+  code = prettyJs(code);
 };
